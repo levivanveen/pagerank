73bc17f5218a36174b74b3251cb9b530df7a6a39
Levi Van Veen <levivanveen2@gmail.com>
2024-03-18 17:11:31 -0400

    Cleaned up the code a bit

--- run_serial.sh
+++ run_serial.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# Navigate to the serial_pagerank folder
-cd serial_pagerank || { echo "Error: serial_pagerank folder not found"; exit 1; }
+SRC_DIR="serial_pagerank"
+INCLUDE_DIR="include"
 
 # Compile the C code
-gcc -o power_method power_method.c matrix_generation.c links.c -lm
+gcc -o "$SRC_DIR/power_method" "$SRC_DIR/power_method.c" "$SRC_DIR/matrix_generation.c" "$SRC_DIR/links.c" -lm -I "$INCLUDE_DIR"
 
 # Check if compilation was successful
 if [ $? -eq 0 ]; then
     echo "Compilation successful. Running the program..."
+    cd "$SRC_DIR"
     # Run the executable
     ./power_method
 else
